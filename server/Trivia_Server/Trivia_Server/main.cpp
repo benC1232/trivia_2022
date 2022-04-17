b8ec0f14e274472e14bb2a1356102fa1158eef98
ben :) <carmonben@gmail.com>
2022-04-17 16:04:22 +0300

    added a main

--- server/Trivia_Server/Trivia_Server/main.cpp
+++ server/Trivia_Server/Trivia_Server/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "WSAInitializer.h"
+#include "Server.h"
+#include <string>
 int main()
 {
+	//took this from week 13
+	WSAInitializer wsa;
+	Server* server = new Server();
+	try
+	{
+		server->run();
+		std::string input = "";
+		while (input != "EXIT")
+		{
+			std::cin >> input;
+		}
+		delete server;
+	}
+	catch (...)
+	{
+		std::cout << "an exception has been thrown" << std::endl;
+		delete server;
+		exit(1);
+	}
 	return 0;
 }
