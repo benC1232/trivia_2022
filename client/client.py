3d01e6e0100c1757e3943edee838b91051040e4e
orim05 <orimeiri05@gmail.com>
2022-04-25 19:21:07 +0300

    changed the client.py file to include the login/signup functions

--- client/client.py
+++ client/client.py
@@ -5,10 +5,10 @@ this file is the client side of the trivia game (v1.0.2)
 it is a simple client that connects to the server and sends the user's choice to the server
 the server will then send back the answer to the user
 """
-
+from typing import List, Set, Dict, Tuple, Optional
 import socket
 import json
-
+import ast
 
 
 
@@ -18,8 +18,6 @@ server_address: tuple= ('127.0.0.1', 1618)
 def sizeOfJson(size: int)->str:
     #returns a string four chars long with the size of the json, if the size is less than 4 chars long it will be padded with 0's
     return str(size).zfill(4)
-
-
 
 def menu() -> tuple:
     choice:int = 0
@@ -36,14 +34,24 @@ def menu() -> tuple:
     return json.dumps(output), choice
 
 
-def build_request(data) -> str:
+def build_request(data: Tuple[str, str]) -> str:
     """
     message builder:
     returns a string in the following format
     |msg code|size of json|json
     """
-    json_data, msg_code= data
-    return str(msg_code) + sizeOfJson(len(json_data)) + json_data.replace(" ","")
+    json_data, msg_code = data
+    return str(msg_code) + sizeOfJson(len(json_data)) + json_data
+
+def unpack_response(data: str) -> Tuple[dict, int]:
+    """
+    message unpacker:
+    returns a tuple of the json and the msg code
+    """
+    msg_code = int(data[0])
+    json_data = json.loads(data[5:])
+    return json_data, msg_code
+
     
 def server_commuinication() -> None:
     """
@@ -63,6 +71,5 @@ def server_commuinication() -> None:
 
 if __name__ == '__main__':
     server_commuinication()
-
 
 
