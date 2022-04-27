97243dedde14d13807cb9e4b984371ea1d991aac
ben :) <carmonben@gmail.com>
2022-04-27 14:47:47 +0300

    added 2 structs

--- server/Trivia_Server/Trivia_Server/HandlerStructs.h
+++ server/Trivia_Server/Trivia_Server/HandlerStructs.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "time.h"
+#include <vector>
+#include "IRequestHandler.h"
+struct RequestResult
+{
+	std::vector<unsigned char> Buffer;
+	IRequestHandler* newHandler;
+};
+struct RequestInfo
+{
+	unsigned char id;
+	time_t receivalTime;
+	std::vector<unsigned char> buffer;
+};
