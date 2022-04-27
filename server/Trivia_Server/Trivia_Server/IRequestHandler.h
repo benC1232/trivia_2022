a267a70dceb8924efb1550241ead88840f047083
ben :) <carmonben@gmail.com>
2022-04-27 17:22:36 +0300

    finished the loginrequesthandler class

--- server/Trivia_Server/Trivia_Server/IRequestHandler.h
+++ server/Trivia_Server/Trivia_Server/IRequestHandler.h
@@ -1,4 +1,8 @@
 #pragma once
+#include "HandlerStructs.h"
 class IRequestHandler
 {
+public:
+	virtual bool isRequestRelevant(RequestInfo request) = 0;
+	virtual RequestResult handleRequest(RequestInfo request) = 0;
 };
