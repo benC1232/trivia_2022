cabdab3523765d83f0fc02089a9f0c1fb42d95d6
ben :) <carmonben@gmail.com>
2022-05-02 19:23:06 +0300

    finished idatabase

--- server/Trivia_Server/Trivia_Server/IDatabase.cpp
+++ server/Trivia_Server/Trivia_Server/IDatabase.cpp
@@ -17,15 +17,56 @@ IDatabase::~IDatabase()
 {
 	delete this->_db;
 }
-
+int userExistCallback(void* data, int argc, char** argv, char** azColName)
+{
+	//cast data to bool
+	data = (bool*)data;
+	if (argc == 0)
+	{
+		*(bool*)data = false;
+	}
+	else
+	{
+		*(bool*)data = true;
+	}
+	return 0;
+}
+int passwordMatchCallback(void* data, int argc, char** argv, char** azColName)
+{
+	//cast data to bool
+	data = (bool*)data;
+	if (argc == 0)
+	{
+		*(bool*)data = false;
+	}
+	else
+	{
+		*(bool*)data = true;
+	}
+	return 0;
+}
 bool IDatabase::doesUserExist(std::string username)
 {
-	return false;
+	bool exists;
+	char* errMessage = nullptr;
+	std::string query = "SELECT * FROM USERS WHERE username = '" + username + "';";
+	int result = sqlite3_exec(this->_db, query.c_str(), userExistCallback, &exists, &errMessage);
+	if (result != SQLITE_OK) {
+		throw std::exception(errMessage);
+	}
+	return exists;
 }
 
 bool IDatabase::doesPasswordMatch(std::string username, std::string password)
 {
-	return false;
+	bool exists;
+	char* errMessage = nullptr;
+	std::string query = "SELECT * FROM USERS WHERE username = '" + username + "' AND password = '" + password + "';";
+	int result = sqlite3_exec(this->_db, query.c_str(), passwordMatchCallback, &exists, &errMessage);
+	if (result != SQLITE_OK) {
+		throw std::exception(errMessage);
+	}
+	return exists;
 }
 
 void IDatabase::addNewUser(std::string username, std::string password, std::string email)
@@ -33,14 +74,15 @@ void IDatabase::addNewUser(std::string username, std::string password, std::stri
 	char* errMessage = nullptr;
 	std::string query = "INSERT INTO Users (username, password, email) VALUES ('" + username + "', '" + password + "', '" + email + "')";
 	const char* sqlStatement = query.c_str();
-	int res = sqlite3_exec(this->_db, sqlStatement, nullptr, nullptr, &errMessage);
-	if (res != SQLITE_OK) {
+	int result = sqlite3_exec(this->_db, sqlStatement, nullptr, nullptr, &errMessage);
+	if (result != SQLITE_OK) {
 		throw std::exception(errMessage);
 	}
 }
 
 void IDatabase::createTables()
 {
+	std::cout << "creating tables" << std::endl;
 	char* errMessage = nullptr;
 	const char* sqlStatement = "CREATE TABLE IF NOT EXISTS USERS ( "
 		"username TEXT PRIMARY KEY,"
