028d0510b4eda1bf64f6ba672f5191f280a73a27
ben :) <carmonben@gmail.com>
2022-05-04 18:31:38 +0300

    added empty class for login manager

---
+++ server/Trivia_Server/Trivia_Server/LoginManager.cpp
@@ -0,0 +1,24 @@
+#include "LoginManager.h"
+
+LoginManager::LoginManager()
+{
+}
+
+LoginManager::~LoginManager()
+{
+}
+
+bool LoginManager::login(std::string username, std::string password)
+{
+	return false;
+}
+
+bool LoginManager::signup(std::string username, std::string password, std::string email)
+{
+	return false;
+}
+
+bool LoginManager::logout(std::string username)
+{
+	return false;
+}
