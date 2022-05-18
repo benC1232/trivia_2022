3d83fcfaf5342d24be81b6b6ef3d2e90a08a3b1e
ben :) <carmonben@gmail.com>
2022-05-18 22:15:35 +0300

    finished the deserializer functions

--- server/Trivia_Server/Trivia_Server/JsonRequestPacketDeserializer.cpp
+++ server/Trivia_Server/Trivia_Server/JsonRequestPacketDeserializer.cpp
@@ -34,18 +34,64 @@ SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(std::vecto
 	parsedData.username = username;
 	return parsedData;
 }
-
+/*
+* function parses the json part of a packet and returns the content of the fields
+* input: vector of unsigned chars (bytes) representing the json string
+* output: the content of the fields in the json
+*/
 GetPlayersInRoomRequest JsonRequestPacketDeserializer::deserializeGetPlayersRequest(std::vector<unsigned char> buffer)
 {
-	return GetPlayersInRoomRequest();
+	std::string jsonString(buffer.begin() + JSON_OFFSET, buffer.end());
+	nlohmann::json jsonObject = nlohmann::json::parse(jsonString);
+	std::string roomId = jsonObject["roomId"];
+	GetPlayersInRoomRequest parsedData;
+	try
+	{
+		parsedData.roomId = atoi(roomId.c_str());
+	}
+	catch (...)
+	{
+		throw std::exception("roomId is not a number");
+	}
+	return parsedData;
 }
 
 JoinRoomRequest JsonRequestPacketDeserializer::deserializeJoinRoomRequest(std::vector<unsigned char> buffer)
 {
-	return JoinRoomRequest();
+	std::string jsonString(buffer.begin() + JSON_OFFSET, buffer.end());
+	nlohmann::json jsonObject = nlohmann::json::parse(jsonString);
+	std::string roomId = jsonObject["roomId"];
+	JoinRoomRequest parsedData;
+	try
+	{
+		parsedData.roomId = atoi(roomId.c_str());
+	}
+	catch (...)
+	{
+		throw std::exception("roomId is not a number");
+	}
+	return parsedData;
 }
 
 CreateRoomRequest JsonRequestPacketDeserializer::deserializeCreateRoomRequest(std::vector<unsigned char> buffer)
 {
-	return CreateRoomRequest();
+	std::string jsonString(buffer.begin() + JSON_OFFSET, buffer.end());
+	nlohmann::json jsonObject = nlohmann::json::parse(jsonString);
+	std::string roomName = jsonObject["roomName"];
+	std::string maxUsers = jsonObject["maxUsers"];
+	std::string questionNumber = jsonObject["questionNumber"];
+	std::string answerTimeout = jsonObject["answerTimeout"];
+	CreateRoomRequest parsedData;
+	try
+	{
+		parsedData.answerTimeout = atoi(answerTimeout.c_str());
+		parsedData.maxUsers = atoi(maxUsers.c_str());
+		parsedData.questionCount = atoi(questionNumber.c_str());
+		parsedData.roomName = roomName;
+	}
+	catch (...)
+	{
+		throw std::exception("maxUsers, questionNumber or answerTimeout is not a number");
+	}
+	return parsedData;
 }
