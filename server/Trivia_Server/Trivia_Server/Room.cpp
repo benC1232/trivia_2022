106986267d25e6f941d5a12090bdf081c2ba51d2
ben :) <carmonben@gmail.com>
2022-05-19 01:01:00 +0300

    added empty functions

--- server/Trivia_Server/Trivia_Server/Room.cpp
+++ server/Trivia_Server/Trivia_Server/Room.cpp
@@ -1,1 +1,18 @@
 #include "Room.h"
+
+Room::Room(RoomData metadata)
+{
+}
+
+void Room::addUser(LoggedUser)
+{
+}
+
+void Room::removeUser(LoggedUser)
+{
+}
+
+std::vector<std::string> Room::getAllUsers()
+{
+	return std::vector<std::string>();
+}
