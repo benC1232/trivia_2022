910ed6c0a8172c637b79cb555ecc4e486b762e93
ben :) <carmonben@gmail.com>
2022-05-19 00:52:51 +0300

    added the room data struct

--- server/Trivia_Server/Trivia_Server/Room.h
+++ server/Trivia_Server/Trivia_Server/Room.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <string>
+//room data struct
+struct RoomData
+{
+	unsigned int id;
+	std::string name;
+	unsigned int maxPlayers;
+	unsigned int numOfQuestionsInGame;
+	unsigned int timePerQuestion;
+	bool isActive;
+};
 class Room
 {
 };
