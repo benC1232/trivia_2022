cdb5d74cb070bc60d45e4da40cd64e5e50c23475
ben :) <carmonben@gmail.com>
2022-05-19 01:23:17 +0300

    added empty functions

--- server/Trivia_Server/Trivia_Server/RoomManager.cpp
+++ server/Trivia_Server/Trivia_Server/RoomManager.cpp
@@ -1,1 +1,23 @@
 #include "RoomManager.h"
+
+RoomManager::RoomManager()
+{
+}
+
+void RoomManager::createRoom(LoggedUser user, RoomData data)
+{
+}
+
+void RoomManager::deleteRoom(int ID)
+{
+}
+
+unsigned int RoomManager::getRoomState(int id)
+{
+	return 0;
+}
+
+std::vector<RoomData> RoomManager::getRooms()
+{
+	return std::vector<RoomData>();
+}
