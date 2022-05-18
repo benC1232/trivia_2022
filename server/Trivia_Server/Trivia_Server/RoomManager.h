2ac4cc6f17675a51180c4a4a8879adc18af7cad3
ben :) <carmonben@gmail.com>
2022-05-19 01:22:37 +0300

    added class feilds+functions

--- server/Trivia_Server/Trivia_Server/RoomManager.h
+++ server/Trivia_Server/Trivia_Server/RoomManager.h
@@ -1,4 +1,16 @@
 #pragma once
+#include <map>
+#include "Room.h"
+#include "LoggedUser.h"
 class RoomManager
 {
+public:
+	RoomManager();
+	void createRoom(LoggedUser user, RoomData data);
+	void deleteRoom(int ID);
+	unsigned int getRoomState(int id);
+	std::vector<RoomData> getRooms();
+
+private:
+	std::map<int, Room> rooms;
 };
