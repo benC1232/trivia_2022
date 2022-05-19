28ca2ba88f6c2c02fd4450a6998a5dcb9d7ff6c7
ben :) <carmonben@gmail.com>
2022-05-19 12:36:36 +0300

    added empty function definitions

--- server/Trivia_Server/Trivia_Server/Question.cpp
+++ server/Trivia_Server/Trivia_Server/Question.cpp
@@ -1,1 +1,16 @@
 #include "Question.h"
+
+std::string Question::getQuestion()
+{
+	return std::string();
+}
+
+std::string Question::getCorrectAnswer()
+{
+	return std::string();
+}
+
+std::vector<std::string> Question::getIncorrectAnswers()
+{
+	return std::vector<std::string>();
+}
