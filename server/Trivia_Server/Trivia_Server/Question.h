6bc66c7d3a41386079c441c0773871b6feab034f
ben :) <carmonben@gmail.com>
2022-05-19 12:35:50 +0300

    added the fields and functions of the question class (the uml was a bit stupid so i fixed it)

--- server/Trivia_Server/Trivia_Server/Question.h
+++ server/Trivia_Server/Trivia_Server/Question.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <string>
+#include <vector>
 //this class is in the uml of V4.0.0 but is required in the IDatabase interface in V2.0.0
 class Question
 {
+public:
+	Question() = default;
+	std::string getQuestion();
+	std::string getCorrectAnswer();
+	std::vector<std::string> getIncorrectAnswers();
+private:
+	std::string m_question;
+	std::vector<std::string> m_answers;
 };
