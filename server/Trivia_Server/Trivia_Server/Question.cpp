a681d2ba0927fc2bf9541c58a05af70a64641737
ben :) <carmonben@gmail.com>
2022-05-19 17:51:53 +0300

    added setters to the question

--- server/Trivia_Server/Trivia_Server/Question.cpp
+++ server/Trivia_Server/Trivia_Server/Question.cpp
@@ -14,3 +14,18 @@ std::vector<std::string> Question::getIncorrectAnswers()
 {
 	return std::vector<std::string>(this->m_answers.begin() + 1, this->m_answers.end());
 }
+
+void Question::setQuestion(std::string question)
+{
+	this->m_question = question;
+}
+
+void Question::setCorrectAnswer(std::string correctAnswer)
+{
+	this->m_answers[0] = correctAnswer;
+}
+
+void Question::setIncorrectAnswers(std::vector<std::string> incorrectAnswers)
+{
+	this->m_answers.insert(this->m_answers.begin() + 1, incorrectAnswers.begin(), incorrectAnswers.end());
+}
--- server/Trivia_Server/Trivia_Server/Question.h
+++ server/Trivia_Server/Trivia_Server/Question.h
@@ -9,6 +9,9 @@ public:
 	std::string getQuestion();
 	std::string getCorrectAnswer();
 	std::vector<std::string> getIncorrectAnswers();
+	void setQuestion(std::string question);
+	void setCorrectAnswer(std::string correctAnswer);
+	void setIncorrectAnswers(std::vector<std::string> incorrectAnswers);
 private:
 	std::string m_question;
 	std::vector<std::string> m_answers;
