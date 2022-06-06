d844ced9c1d502fe0249228ea07f3a97eaed816a
ben :) <carmonben@gmail.com>
2022-06-06 14:36:41 +0300

    finished everything except for the private functions

--- server/Trivia_Server/Trivia_Server/GameRequestHandler.cpp
+++ server/Trivia_Server/Trivia_Server/GameRequestHandler.cpp
@@ -1,5 +1,8 @@
 #include "GameRequestHandler.h"
-
+#define LEAVE_GAME_CODE 14
+#define GET_QUESTION_CODE 15
+#define SUBMIT_ANSWER_CODE 16
+#define GET_GAME_RESULT_CODE 17
 GameRequestHandler::GameRequestHandler(GameManager* gameManager, LoggedUser loggedUser, RequestHandlerFactory* requestHandlerFactory, Game* game)
 {
 	this->m_gameManager = gameManager;
@@ -14,12 +17,36 @@ GameRequestHandler::~GameRequestHandler()
 
 bool GameRequestHandler::isRequestRelevant(RequestInfo requestInfo)
 {
-	return false;
+	return requestInfo.id == LEAVE_GAME_CODE || requestInfo.id == GET_QUESTION_CODE || requestInfo..id == SUBMIT_ANSWER_CODE || requestInfo.id == GET_GAME_RESULT_CODE;
 }
 
 RequestResult GameRequestHandler::handleRequest(RequestInfo requestInfo)
 {
-	return RequestResult();
+	RequestResult result;
+	if (requestInfo.id == LEAVE_GAME_CODE)
+	{
+		result = leaveGame(requestInfo);
+	}
+	else if (requestInfo.id == GET_QUESTION_CODE)
+	{
+		result = getQuestion(requestInfo);
+	}
+	else if (requestInfo.id == SUBMIT_ANSWER_CODE)
+	{
+		result = submitAnswer(requestInfo);
+	}
+	else if (requestInfo.id == GET_GAME_RESULT_CODE)
+	{
+		result = getGameResults(requestInfo);
+	}
+	else
+	{
+		ErrorResponse num;
+		num.message = "error while handling request [room admin request handler has recived a wrong code]";
+		result.buffer = JsonResponsePacketSerializer::serializeErrorResponse(num);
+		result.newHandler = nullptr;
+	}
+	return result;
 }
 
 RequestResult GameRequestHandler::getQuestion(RequestInfo requestInfo)
