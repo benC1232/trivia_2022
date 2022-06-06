55e2552fa7a763dbf2425c69d8f7eb34d9caf30b
orim05 <orimeiri05@gmail.com>
2022-06-06 14:15:46 +0300

    added the game and the post game screens

---
+++ client/TriviaClient/TriviaClient/gameWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace TriviaClient
+{
+    /// <summary>
+    /// Interaction logic for gameWindow.xaml
+    /// </summary>
+    public partial class gameWindow : Window
+    {
+
+        public gameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void Answer1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Answer2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Answer3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Answer4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Leave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
