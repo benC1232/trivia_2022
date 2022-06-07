93040b0481cca40cb09535e4eedc834c989ae3b7
ben :) <carmonben@gmail.com>
2022-06-07 19:24:24 +0300

    added a leave feature

--- client/TriviaClient/TriviaClient/PostGameWindow.xaml.cs
+++ client/TriviaClient/TriviaClient/PostGameWindow.xaml.cs
@@ -44,6 +44,22 @@ namespace TriviaClient
 
         private void Leave_Click(object sender, RoutedEventArgs e)
         {
+            byte[] arr = new byte[1];
+            arr[0] = 1;
+            this.comm.Send(14, arr);
+            Tuple<int, byte[]> response = this.comm.Recieve();
+            if (response.Item1 == 14)
+            {
+                MenuWindow menuWindow = new MenuWindow(this.comm);
+                this.Close();
+                menuWindow.Show();
+            }
+            else if (response.Item1 == 3)
+            {
+                responseStructs.ErrorResponse errResponse = JsonConvert.DeserializeObject<responseStructs.ErrorResponse>(Encoding.ASCII.GetString(response.Item2));
+                this.errorLbl.Visibility = Visibility.Visible;
+                this.errorLbl.Text = errResponse.message;
+            }
         }
     }
 }
