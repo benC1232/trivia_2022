c0683f5ed35181d0078143ea80844a1670c49a77
orim05 <orimeiri05@gmail.com>
2022-06-10 18:17:18 +0300

    idontmore

--- server/Trivia_Server/Trivia_Server/Game.cpp
+++ server/Trivia_Server/Trivia_Server/Game.cpp
@@ -16,7 +16,7 @@ Game::Game(std::vector<Question> questions, std::map<LoggedUser, GameData> playe
 Question Game::getQuestionForUser(LoggedUser user)
 {
 	Question q;
-	q.setQuestion("");
+	q.setQuestion("Err q");
 	if (this->m_players.at(user).currentQuestion.getQuestion() == q.getQuestion()) {
 		this->m_players.at(user).currentQuestion = this->m_questions[0];
 		return this->m_questions[0];
@@ -29,6 +29,7 @@ Question Game::getQuestionForUser(LoggedUser user)
 		}
 		idx++;
 	}
+	if (idx == this->m_questions.size()) throw(std::exception("no more questions"));
 	this->m_players.at(user).currentQuestion = this->m_questions.at(idx + 1);
 	return this->m_questions.at(idx + 1);
 }
